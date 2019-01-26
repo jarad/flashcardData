0da624cc269bd9eed87022beb7d069ffaca1b2a5
jarad <niemi@iastate.edu>
2019-01-26 15:12:53 -0600

    Update script for use with 2018 data
    - Update package data
    - Update plot

--- data-raw/flashcards.R
+++ data-raw/flashcards.R
@@ -1,5 +1,5 @@
-library(dplyr)
-library(tidyr)
+library("dplyr")
+library("tidyr")
 
 my_read_csv = function(f, into) {
   readr::read_csv(f) %>%
@@ -15,23 +15,36 @@ read_dir = function(path, pattern, into) {
   plyr::ldply(files, my_read_csv, into = into)
 }
 
+
+
 flashcards <- read_dir(path = "flashcards",
               pattern = "*.csv",
               into = c("dir","ymd","extension")) %>%
   select(-dir, -ymd, -extension) %>%
 #  na.omit() %>%
 
-  mutate(date = as.Date(date),
+  mutate(date = lubridate::ymd(date),
 
-         minutes = as.numeric(substr(time, 1, 1)) +
-           ifelse(nchar(time)<4, 0, as.numeric(substr(time, 3, 4)) / 60),
+         # Time was either 5:00 for 5 minutes or # of seconds
+         seconds = time,
 
+         # Count is either count or n_cards
+         count = ifelse(is.na(count), n_cards, count),
+
+         # Student is either student or child
+         student = ifelse(is.na(student), child, student),
+
+         # card is operation
+         card = ifelse(is.na(card), operation, card),
          card = tolower(card)) %>%
 
   rename(synonym = card) %>%
   left_join(readr::read_csv("card_synonyms.csv"), by="synonym") %>%
+  mutate(card = factor(card,
+                       levels = c("addition","subtraction",
+                                  "multiplication","division"))) %>%
 
-  select(-time, -synonym)
+  select(date, student, card, seconds, count)
 
 
 devtools::use_data(flashcards, overwrite = TRUE)
